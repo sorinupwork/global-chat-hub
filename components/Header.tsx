a69740c30167cdcdc644b0d09b76a13afff3adde
sorinupwork <sorin.upwork@gmail.com>
2023-12-06 02:48:28 +0200

    configure project with shadcn/ui and add modeToggle

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Logo from './Logo';
+import DarkModeToggle from './DarkModeToggle';
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          {/* Language Select */}
+
+          {/* Session && (
+            ...
+          ) */}
+
+          {/* DarkMode Toggle */}
+          <DarkModeToggle />
+
+          {/* User Button */}
+        </div>
+      </nav>
+
+      {/* Upgrade Banner */}
+    </header>
+  );
+}
+
+export default Header;
