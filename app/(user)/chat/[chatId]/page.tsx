258f2a59aa45860296be43e0d389a9d3b568ef0c
sorinupwork <sorin.upwork@gmail.com>
2023-12-11 01:20:55 +0200

    adding chat page layout and functionality to write a message

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { getServerSession } from 'next-auth';
+import { getDocs } from 'firebase/firestore';
 
-function ChatPage() {
-  return <></>;
+import { authOptions } from '@/auth';
+import ChatInput from '@/components/ChatInput';
+import { sortedMessagesRef } from '@/lib/converters/Message';
+import ChatMessages from '@/components/ChatMessages';
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+async function ChatPage({ params: { chatId } }: Props) {
+  const session = await getServerSession(authOptions);
+
+  const initialMessages = (await getDocs(sortedMessagesRef(chatId))).docs.map(
+    (doc) => doc.data()
+  );
+
+  return (
+    <>
+      {/* Admin Controls */}
+      {/* ChatMembersBadge */}
+
+      <div className="flex-1" style={{ minHeight: 'var(--min-height)' }}>
+        <ChatMessages
+          chatId={chatId}
+          session={session}
+          initialMessages={initialMessages}
+        />
+      </div>
+
+      <ChatInput chatId={chatId} />
+    </>
+  );
 }
 
 export default ChatPage;
