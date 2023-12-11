e4d3ffb0029ce7ee557a3cdfcbd3a780f69d4584
sorinupwork <sorin.upwork@gmail.com>
2023-12-12 01:08:25 +0200

    fix mobile responsiveness

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -13,7 +13,7 @@ export default function ChatsLayout({
   return (
     <div
       className={`flex-1 w-full flex flex-col max-w-6xl mx-auto sm:min-h-[800px] ${
-        isPro && 'sm:min-h-[910px]'
+        isPro && 'sm:min-h-[840px]'
       } min-h-[671px] ${isPro && 'min-h-[712px]'}`}
     >
       {children}
