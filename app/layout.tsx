b1ff15a9179ead1d72fa04aae44e030eafcd9699
sorinupwork <sorin.upwork@gmail.com>
2023-12-12 03:30:56 +0200

    change metadata thumbnail image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Head from 'next/head';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
@@ -16,6 +15,9 @@ export const metadata: Metadata = {
   title: 'Global Chat Hub',
   description:
     'Global Chat Hub is a cutting-edge Software as a Service (SaaS) platform designed to revolutionize communication by seamlessly breaking down language barriers. This innovative platform serves as a universal meeting point for individuals and businesses worldwide, offering a comprehensive chat translation feature that facilitates smooth and effective conversations in any language.',
+  openGraph: {
+    images: ['https://i.ibb.co/RQng9hY/DemoGif.jpg'],
+  },
   authors: [{ name: 'Sorin C Adam', url: 'https://sorin-adam.netlify.app' }],
   keywords: [
     'global',
@@ -37,12 +39,6 @@ export default function RootLayout({
     <ClientProviders>
       <html lang="en">
         <body className={inter.className}>
-          <Head>
-            <meta
-              property="og:image"
-              content={'https://i.ibb.co/wzJLQmx/global-chat-hub.png' || ''}
-            />
-          </Head>
           <FirebaseAuthProvider>
             <SubscriptionProvider>
               <ThemeProvider
